43bb7287cc76abc0834efbded721447007aca47e
Aneiv <161586104+Aneiv@users.noreply.github.com>
2024-02-28 11:39:30 +0100

    Changes
    test

--- hello world.c
+++ hello world.c
@@ -8,7 +8,15 @@ int main() {
 	printf("Podaj liczbe: ");
 	scanf("%d", &a);
 	printf("Twoja liczba to: %d", a);
-
+	
+	int b;
+	printf("Podaj 2 liczbe: ");
+	scanf("%d", &b);
+	printf("Twoja liczba to: %d", b);
+	int c;
+	c = a*b;
+	printf("Iloczyn tych liczb to: %d", c);	
+	
 	
 	return 0;
 }
